33249f466995088d03b3a3d7655353b3721e422c
Maya <maya_cherneva.0802@abv.bg>
2017-05-29 22:24:38 +0300

    New Folder - Exercises Lists

---
+++ Fundamentals/Exercises Lists/ElementsAtOddPositions/ElementsAtOddPositions/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElementsAtOddPositions
+{
+    class Program
+    {
+        static void Main()
+        {
+            var someText = Console.ReadLine().Split(' ').ToList();
+            var result = new List<string>();
+
+            for (int i = 1; i < someText.Count; i++)
+            { 
+                if (i % 2 != 0)
+                {
+                    result.Add(someText[i]);
+                }
+            }
+            Console.WriteLine(String.Join("", result));
+        }
+    }
+}
